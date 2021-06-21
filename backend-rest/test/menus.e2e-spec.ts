bda8d8304241bc7a5137a6cf37bd4878afda68f8
Nattharika Romsanuk <nattharika@livingmobile.me>
2021-06-21 18:15:55 +0700

    Edited UnitTest
     • categories
     • stores
     • menus

--- backend-rest/test/menus.e2e-spec.ts
+++ backend-rest/test/menus.e2e-spec.ts
@@ -103,7 +103,7 @@ describe('MenusController (e2e)', () => {
                     
                 };
                 await service.create(CreateMenu)
-                console.log(CreateMenu)
+
                 await request(app.getHttpServer())
                 .post('/menus')
                 .send(CreateMenu)
@@ -111,7 +111,6 @@ describe('MenusController (e2e)', () => {
                 .then((response) => {
                     expect(response.statusCode).toEqual(201);
                     });
-                  
     });
 
 });
@@ -119,48 +118,145 @@ describe('MenusController (e2e)', () => {
 
         describe('Update Menus', () => {
             it('When menu with valid input, then response 200 (OK) with update menus', async () => {
-                const updateMenuInput = {
-                    id: '40c4949b-9feb-477c-af71-7424c39460ff',
-                    name: 'UpdateMenu',
-                    price: 200,
-                    categoryId : '501bbfff-40fd-4d70-b9ca-d4fc07de5e37'
+                 //CreateStore
+                 const StoreInsert = {
+                    name: 'TestInSert',
+                    description: 'Test',
+                    rating: 5,
                 };
-                return request(app.getHttpServer())
-                    .put('/menus/{id}')
-                    .send(updateMenuInput)
+                await storeService.create(StoreInsert);
+                //decare ID
+                let id = ''
+                await request(app.getHttpServer())
+                    .get('/stores')
+                    .expect(200)
+                    .then((response) => {
+                       id = response.body[0].id
+                    })
+                 //CreateCategory   
+                    const CreateCategory1 = {
+                        name : 'TestCat',
+                        storeId : id
+                    }
+                    await categoryService.create(CreateCategory1)
+                    const CreateCategory2 = {
+                        name: 'new test',
+                        storeId: id,
+                    };
+                    await categoryService.create(CreateCategory2)
+    
+                    const CreateCategory3 = {
+                        name: 'new test',
+                        storeId: id,
+                    };
+                    await categoryService.create(CreateCategory3)
+                    let Catid=''
+                    await request(app.getHttpServer())
+                    .get('/categories')
+                    .expect(200)
+                    .then((response) => {
+                        Catid = response.body[0].id
+                    })
+
+                const CreateMenu = {
+                    name: 'Test',
+                    price : 150,
+                    categoryId: Catid
+                    
+                };
+                await service.create(CreateMenu)
+                let MenuId = '';
+                await request(app.getHttpServer())
+                .post('/menus')
+                .send(CreateMenu)
+                .then((response) => {
+                    MenuId = response.body.id
+                    });
+                    const UpdateMenu = {
+                        name: 'UpdateMenu',
+                        price: 100,
+                        categoryId : Catid
+                        };
+                        return request(app.getHttpServer())
+                        .put(`/menus/${MenuId}`)
+                        .send(UpdateMenu)
+                        .expect(200)
+                        .then((response) => {
+                            expect(response.statusCode).toEqual(200);
+                        });
+
+
+             
+             });
+             
+            describe('Delete menus', () => {
+                it('When store with valid input, then response 200 (OK) with deleted stores', async () => {
+                        //CreateStore
+                 const StoreInsert = {
+                    name: 'TestInSert',
+                    description: 'Test',
+                    rating: 5,
+                };
+                await storeService.create(StoreInsert);
+                //decare ID
+                let id = ''
+                await request(app.getHttpServer())
+                    .get('/stores')
+                    .expect(200)
+                    .then((response) => {
+                       id = response.body[0].id
+                    })
+                 //CreateCategory   
+                    const CreateCategory1 = {
+                        name : 'TestCat',
+                        storeId : id
+                    }
+                    await categoryService.create(CreateCategory1)
+                    const CreateCategory2 = {
+                        name: 'new test',
+                        storeId: id,
+                    };
+                    await categoryService.create(CreateCategory2)
+    
+                    const CreateCategory3 = {
+                        name: 'new test',
+                        storeId: id,
+                    };
+                    await categoryService.create(CreateCategory3)
+                    let Catid=''
+                    await request(app.getHttpServer())
+                    .get('/categories')
+                    .expect(200)
+                    .then((response) => {
+                        Catid = response.body[0].id
+                    })
+
+                const CreateMenu = {
+                    name: 'Test',
+                    price : 150,
+                    categoryId: Catid
+                    
+                };
+                await service.create(CreateMenu)
+                let MenuId = '';
+                await request(app.getHttpServer())
+                .post('/menus')
+                .send(CreateMenu)
+                .then((response) => {
+                    MenuId = response.body.id
+                    });
+                    await request(app.getHttpServer())
+                    .delete(`/menus/${MenuId}`)
                     .expect(200)
                     .then((response) => {
                         expect(response.statusCode).toEqual(200);
                         });
-                });
-                it('When update invalid , then response 400 (Bad Request)', async () => {
-                    // arrange
-                    const createStoreInput = { name : '', description: '' ,rating :'test' };
-        
-                    return request(app.getHttpServer())
-                        .put('/stores/{id}')
-                        .send(createStoreInput)
-                        .expect(400);
-                });
-            });
-
-
-            describe('Delete menus', () => {
-                it('When store with valid input, then response 200 (OK) with deleted stores', async () => {
-                    // arrange
-                    const DeleteID = {
-                        id: '40c4949b-9feb-477c-af71-7424c39460ff'
-                    };
-                    return request(app.getHttpServer())
-                        .delete('/stores/{id}')
-                        .send(DeleteID)
-                        .expect(200)
-                        .then((response) => {
-                            expect(response.statusCode).toEqual(200);
-                            });
+                    
                     });
                 });
-                afterAll(async () => {
-                    await app.close();
-                });
+               
+            });
+            afterAll(async () => {
+                await app.close();
+            });
 });
